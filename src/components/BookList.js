e097ad243ea9e6cd2adaa7ca959ff866c6d1622c
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-10-29 16:36:47 -0500

    access context in navbar via Context.Consumer

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
-  static contextType = ThemeContext;
+  static contextType = ThemeContext; // looks up component tree for the first time it finds a provider for this context (finds it one level up in App.js --> gives access to the data provided by this that we put in the value property)
   render() {
     const { isLightTheme, light, dark } = this.context;
     const theme = isLightTheme ? light : dark;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-  static contextType = ThemeContext; // looks up component tree for the first time it finds a provider for this context (finds it one level up in App.js --> gives access to the data provided by this that we put in the value property)
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
 
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
